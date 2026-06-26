d1d978bd7cfa734699c5d329c0afef91b35b17d6
himanshukhare <himanshukhare43071@gmail.com>
2026-06-27 00:51:08 +0530

    Center footer copyright

--- lib/features/footer/app_footer.dart
+++ lib/features/footer/app_footer.dart
@@ -13,10 +13,6 @@ class AppFooter extends StatelessComponent {
           div(classes: 'app-footer__logo', [Component.text(AppConstants.brandName)]),
           div(classes: 'app-footer__copyright', [Component.text(AppConstants.copyright)]),
         ]),
-        div(classes: 'app-footer__links', [
-          for (var i = 0; i < AppConstants.socialLabels.length; i++)
-            a(href: AppConstants.socialUrls[i], classes: 'app-footer__link', [Component.text(AppConstants.socialLabels[i])]),
-        ]),
       ]),
     ]);
   }
